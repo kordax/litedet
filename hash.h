7e62121c409ffe51afe679bf725e1c5e37575149
kordax <dremor8686@mail.ru>
2014-12-23 01:56:04 +0300

    md5 hash_get_n

--- hash.h
+++ hash.h
@@ -17,16 +17,28 @@
 #define _MD5_APPEND_BITS 0x80
 #define _SIGN_MAX_SIZE 8192
 
+int hash_get_n(size_t size)
+{
+
+}
+
 int gen_hash(char *input)
 {
     unsigned int size = 0;
     unsigned int i = 0;
+    unsigned int N = 0;
     while(input[i] != NULL)
     {
         i++;
         size++;
     }
     if (size == 0) return -1;
+
+    while (size != 512 * N + 448)
+    {
+
+        input[size + i] =
+    }
 
     return 0;
 }
